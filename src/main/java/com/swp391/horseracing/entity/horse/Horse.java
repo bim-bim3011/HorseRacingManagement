d8fd0488308935a873db3a1cbc915225f45b57c7
Mimi6868 <dattqse181591@fpt.edu.vn>
2026-06-16 16:03:45 +0700

    ...

--- src/main/java/com/swp391/horseracing/entity/horse/Horse.java
+++ src/main/java/com/swp391/horseracing/entity/horse/Horse.java
@@ -32,8 +32,8 @@ public class Horse {
     @Column(name = "health_status", length = 100)
     private String healthStatus;
 
-    @Column(name = "certificate_url", length = 500)
-    private String certificateUrl;  // ← thêm URL chứng chỉ
+    @Column(name = "health_certificate_url", length = 500)
+    private String healthCertificateUrl; // lưu url của giấy khám sk
 
     @Column
     private Integer age;  // ← tuổi ngựa để kiểm tra tiêu chuẩn race
