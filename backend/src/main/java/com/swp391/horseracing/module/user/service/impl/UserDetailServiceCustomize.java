69d253aaad14b735f7f091200e085dab293491ae
bim-bim3011 <dangkhanh1130@gmail.com>
2026-07-15 15:55:42 +0700

    update Logic authenticate user with both email and username

--- backend/src/main/java/com/swp391/horseracing/module/user/service/impl/UserDetailServiceCustomize.java
+++ backend/src/main/java/com/swp391/horseracing/module/user/service/impl/UserDetailServiceCustomize.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+import com.swp391.horseracing.module.user.entity.User;
 
 @Service
 @RequiredArgsConstructor
@@ -18,12 +20,22 @@ import org.springframework.stereotype.Service;
 public class UserDetailServiceCustomize implements UserDetailsService{
 
     UserRepository userRepository;
+    
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       return userRepository.findByUsername(username)
-               .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+    public UserDetails loadUserByUsername(String input) throws UsernameNotFoundException {
+        if (EMAIL_PATTERN.matcher(input).matches()) {
+            User user = userRepository.findByEmail(input);
+            if (user == null) {
+                throw new AppException(ErrorCode.USER_NOT_FOUND);
+            }
+            return user;
+        } else {
+            return userRepository.findByUsername(input)
+                   .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        }
     }
-
 
 }
